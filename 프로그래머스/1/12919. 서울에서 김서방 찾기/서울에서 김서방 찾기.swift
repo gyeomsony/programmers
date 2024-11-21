6680e49b8cfc9d2fb3623ac42c478e1aeba3cb6d
sonny <gyeomsony@gmail.com>
2024-11-21 09:41:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -1,6 +1,5 @@
 func solution(_ seoul:[String]) -> String {
-   if let answer = seoul.firstIndex(of: "Kim") {
-       return "김서방은 \(answer)에 있다"
-   }
-    return ""
-}
+  let index = seoul.firstIndex(of: "Kim")! 
+    return "김서방은 \(index)에 있다"
+  }
+    
