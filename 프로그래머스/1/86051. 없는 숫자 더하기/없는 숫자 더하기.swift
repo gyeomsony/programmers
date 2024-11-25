74bae2e399badc831a01e60c2875326d4943f36b
sonny <gyeomsony@gmail.com>
2024-11-25 09:54:12 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -1,8 +1,4 @@
 import Foundation
 func solution(_ numbers:[Int]) -> Int {
-    let allNum = Set(0...9)
-    let giveNum = Set(numbers)
-    let missingNum = allNum.subtracting(giveNum)
-    
-    return missingNum.reduce(0, +)
+    return 45-numbers.reduce(0, +)
 }
