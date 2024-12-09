d138cb477595fb7bdc6090f419966028e51c13a3
sonny <gyeomsony@gmail.com>
2024-12-09 09:22:03 +0900

    [level 0] Title: A 강조하기, Time: 0.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
@@ -1,15 +1,6 @@
 import Foundation
 
-func solution(_ myString: String) -> String {
-    return myString.map { char -> Character in
-        if char == "a" {
-            return "A"
-        } else if char.isUppercase && char != "A" {
-            return Character(char.lowercased())
-        } else {
-            return char
-        }
-    }.reduce("") { $0 + String($1) }
-}
-
+func solution(_ myString:String) -> String {
+    return myString.lowercased().replacingOccurrences(of: "a", with: "A")
+} 
 
