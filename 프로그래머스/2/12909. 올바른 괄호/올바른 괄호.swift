bf19f5b9b969a6362dd3ca6445408c2579753dbc
sonny <gyeomsony@gmail.com>
2024-12-13 09:32:15 +0900

    [level 2] Title: 올바른 괄호, Time: 11.26 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ s: String) -> Bool {
+    var stack = [Character]()
+
+    for char in s {
+        if char == "(" {
+            stack.append(char)
+        } else if char == ")" {
+            if stack.isEmpty {
+                return false 
+            }
+            stack.removeLast() 
+        }
+    }
+
+    return stack.isEmpty 
+}
