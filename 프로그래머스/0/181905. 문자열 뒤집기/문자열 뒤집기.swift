172fb1ade79402523900f36d457b2167eb1ef8a2
sonny <gyeomsony@gmail.com>
2024-12-16 09:36:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.35 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.swift
@@ -1,12 +1,9 @@
 import Foundation
 
-func solution(_ my_string: String, _ s: Int, _ e: Int) -> String {
-    let startIndex = my_string.index(my_string.startIndex, offsetBy: s)
-    let endIndex = my_string.index(my_string.startIndex, offsetBy: e)
-    
-    let prefix = my_string[..<startIndex]
-    let revers = my_string[startIndex...endIndex].reversed()
-    let suffix = my_string[my_string.index(after: endIndex)...]
-    
-    return String(prefix) + String(revers) + String(suffix)
+func solution(_ my_string:String, _ s:Int, _ e:Int) -> String {
+    var result = Array(my_string)
+
+    result[s...e].reverse()
+
+    return String(result)
 }
